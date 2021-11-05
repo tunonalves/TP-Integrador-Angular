d5e6d51bf9e0729e81e1bff906719651f552fa12
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-11-05 19:06:30 -0300

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductAMDComponent } from './ProductManagement/product-amd/product-amd.component';
 import { ProductListComponent } from './ProductManagement/product-list/product-list.component';
-import { UserstListComponent } from './UsersManagement/userst-list/userst-list.component';
-import { UsersamdComponent } from './UsersManagement/usersamd/usersamd.component';
+import { UserstListComponent } from './UsersManagement/users-list/userst-list.component';
+import { UsersamdComponent } from './UsersManagement/users-amd/usersamd.component';
 import { DashboardComponent } from './Main/dashboard/dashboard.component';
 import { CategoryProductListComponent } from './ProductManagement/category-product-list/category-product-list.component';
 import { CategoryProductAmdComponent } from './ProductManagement/category-product-amd/category-product-amd.component';
-import { UsersTypeListComponent } from './ProductManagement/users-type-list/users-type-list.component';
-import { UsersTypeAmdComponent } from './ProductManagement/users-type-amd/users-type-amd.component';
+import { UsersTypeListComponent } from './UsersManagement/users-type-list/users-type-list.component';
+import { UsersTypeAmdComponent } from './UsersManagement/users-type-amd/users-type-amd.component';
 
 @NgModule({
   declarations: [
